a9cfcccaf0a6ccc05c433e2a0e100c862921600e
vedant-3010 <vedantdandgepro@gmail.com>
2023-07-02 18:38:27 +0530

    theme changes

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import { ThemeProvider } from "styled-components";
 
 const lightTheme = {
   backgroundColor: "#f5f5fd",
   secondBackground: "#ffffff",
-  borderColor: "#e7e3fa",
+  borderColor: "#9BA4B5",
   navBackground: "rgba(245, 241, 241, 0.3)",
   headerColor: "#181818",
-  textColor: "#696969",
-  mainColor: "#8871e6",  
-  secondColor: "#dbd2ff",
-  gradientColor: "linear-gradient(45deg, #8871e6, #5a43b9)",
+  textColor: "#212A3E",
+  mainColor: "#212A3E",  
+  secondColor: "#9BA4B5",
+  gradientColor: "linear-gradient(45deg, #394867, #212A3E)",
   boxShadow: "8px 8px 18px #d4d4d4, -8px -8px 18px #ffffff",
-  keyword: "blue",
+  keyword: "#B31312",
   className: "#78b3da",
 };
 
 const darkTheme = {
-  backgroundColor: "#0F172A",
-  secondBackground: "#121b31",
-  borderColor: "#0d1324",
-  navBackground: "rgba(13, 25, 48, 0.3)",
-  headerColor: '#e0e7fd',
-  textColor: "#8493A8",
-  mainColor: "#5CE8D2",
-  secondColor: "#122B39",
-  gradientColor: "linear-gradient(45deg, #5CE8D2, #48c2b0)",
-  boxShadow: "8px 8px 18px #0F172A, -8px -8px 18px #0F172A",
-  keyword: "#326cb8",
-  className: "#389d89"
+  backgroundColor: "#1A1A1A",
+  secondBackground: "#121212",
+  borderColor: "#0D0D0D",
+  navBackground: "rgba(13, 13, 13, 0.7)",
+  headerColor: "#F9F9F9",
+  textColor: "#A6A6A6",
+  mainColor: "#FFD700", // Gold
+  secondColor: "#121212", // Goldenrod
+  gradientColor: "linear-gradient(45deg, #FFD700, #DAA520)",
+  boxShadow: "8px 8px 18px rgba(0, 0, 0, 0.5), -8px -8px 18px #0D0D0D",
+  keyword: "#EE82EE", // Violet
+  className: "#FF1493", // Deep Pink
 };
 
 function Theme({ children, theme }) {
-  return <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+      {children}
+    </ThemeProvider>
+  );
 }
 
 export default Theme;
