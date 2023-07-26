062fac6f3cf4d329d0e0d812866816c6c070bde9
vedant-3010 <vedantdandgepro@gmail.com>
2023-07-26 12:26:07 +0530

    minor changes

--- src/assets/img/Wave.js
+++ src/assets/img/Wave.js
@@ -1,6 +1,8 @@
+// Wave.js
+import React from "react";
 import styled from "styled-components";
-import "../img/styles.css";
 import { motion } from "framer-motion";
+import "../img/styles.css"
 
 const draw = {
   hidden: { pathLength: 0, opacity: 0 },
@@ -17,75 +19,13 @@ const draw = {
   }
 };
 
-function Wave() {
+const Wave = () => {
   return (
-    <motion.svg
-      viewBox="0 0 600 600"
-      initial="hidden"
-      animate="visible"
-    >
-      <motion.circle
-        cx="100"
-        cy="100"
-        r="80"
-        stroke="#ff0055"
-        variants={draw}
-        custom={1}
-      />
-      <motion.line
-        x1="220"
-        y1="30"
-        x2="360"
-        y2="170"
-        stroke="#00cc88"
-        variants={draw}
-        custom={2}
-      />
-      <motion.line
-        x1="220"
-        y1="170"
-        x2="360"
-        y2="30"
-        stroke="#00cc88"
-        variants={draw}
-        custom={2.5}
-      />
-      <motion.rect
-        width="140"
-        height="140"
-        x="410"
-        y="30"
-        rx="20"
-        stroke="#0099ff"
-        variants={draw}
-        custom={3}
-      />
-      <motion.circle
-        cx="100"
-        cy="300"
-        r="80"
-        stroke="#0099ff"
-        variants={draw}
-        custom={2}
-      />
-      <motion.line
-        x1="220"
-        y1="230"
-        x2="360"
-        y2="370"
-        stroke="#ff0055"
-        custom={3}
-        variants={draw}
-      />
-      <motion.line
-        x1="220"
-        y1="370"
-        x2="360"
-        y2="230"
-        stroke="#ff0055"
-        custom={3.5}
-        variants={draw}
-      />
+    <WaveSvg viewBox="0 0 600 600" initial="hidden" animate="visible">
+      
+      <motion.circle cx="100" cy="300" r="80" stroke="#0099ff" variants={draw} custom={2} />
+      <motion.line x1="220" y1="230" x2="360" y2="370" stroke="#ff0055" custom={3} variants={draw} />
+      <motion.line x1="220" y1="370" x2="360" y2="230" stroke="#ff0055" custom={3.5} variants={draw} />
       <motion.rect
         width="140"
         height="140"
@@ -96,32 +36,9 @@ function Wave() {
         custom={4}
         variants={draw}
       />
-      <motion.circle
-        cx="100"
-        cy="500"
-        r="80"
-        stroke="#00cc88"
-        variants={draw}
-        custom={3}
-      />
-      <motion.line
-        x1="220"
-        y1="430"
-        x2="360"
-        y2="570"
-        stroke="#0099ff"
-        variants={draw}
-        custom={4}
-      />
-      <motion.line
-        x1="220"
-        y1="570"
-        x2="360"
-        y2="430"
-        stroke="#0099ff"
-        variants={draw}
-        custom={4.5}
-      />
+      <motion.circle cx="100" cy="500" r="80" stroke="#00cc88" variants={draw} custom={3} />
+      <motion.line x1="220" y1="430" x2="360" y2="570" stroke="#0099ff" variants={draw} custom={4} />
+      <motion.line x1="220" y1="570" x2="360" y2="430" stroke="#0099ff" variants={draw} custom={4.5} />
       <motion.rect
         width="140"
         height="140"
@@ -132,16 +49,16 @@ function Wave() {
         variants={draw}
         custom={5}
       />
-    </motion.svg>
+    </WaveSvg>
   );
-}
+};
 
-
-const WaveSvg = styled.svg`
+const WaveSvg = styled(motion.svg)`
   z-index: -1;
   position: absolute;
-  left: 0;
-  stroke: ${props => props.theme.mainColor};
+  right: 0;
+  bottom: 0;
+  opacity : 0.2;
 `;
 
 export default Wave;
